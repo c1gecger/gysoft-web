003e539bf8a6054759d8589c78fd2fc644a800fa
Aranyos Zalán <aranyos.zalan@gmail.com>
2026-09-23 09:24:01 +0200

    Footer
    Footer elkészítése

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 // import Fields from "./components/Fields";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Banner />
+      <Footer />
       {/* <Fields /> */}
     </>
   );
